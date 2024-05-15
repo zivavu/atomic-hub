f07bb18815e3574e38f1c3edcaface2301cdea61
zivavu <zivavu@gmail.com>
2024-05-15 12:03:15 +0200

    :tada: Add atomic manifest

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import cloudflare from '@astrojs/cloudflare';
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [svelte(), tailwind()],
+	output: 'server',
+	adapter: cloudflare(),
+});
