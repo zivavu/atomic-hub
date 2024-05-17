9d937d3db8397f3497f04ceb34adc76b17947cf5
zivavu <zivavu@gmail.com>
2024-05-17 22:34:39 +0200

    :lipstick: Improve mobile

--- src/components/ArticlesFeed/ArticlesFeed.svelte
+++ src/components/ArticlesFeed/ArticlesFeed.svelte
@@ -19,10 +19,8 @@
 	});
 </script>
 
-<div class="px-12 py-8">
+<main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
 	<TagInput />
 	<SelectedTags />
-	<main>
-		<ArticleList />
-	</main>
-</div>
+	<ArticleList />
+</main>
