e4cb2f8051e12322bfbc5108782ff18b663e63b7
zivavu <zivavu@gmail.com>
2024-05-18 18:31:43 +0200

    :speech_balloon: Add new articles

--- src/components/ArticlesFeed/types/Article.ts
+++ src/components/ArticlesFeed/types/Article.ts
@@ -1,9 +1,2 @@
-import type { InferEntrySchema } from 'astro:content';
 
-export type Article = {
-	slug: string;
-	id: string;
-	body: string;
-	data: InferEntrySchema<'articles'>;
-	collection: string;
-};
+
